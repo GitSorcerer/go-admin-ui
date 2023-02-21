35a8695d205e22f3575c0a3d64cf77cb0c64752d
高建成 <gaojiancheng@shwpg.com>
2023-02-21 18:18:37 +0800

    添加字段 content

--- docs/sql/blogs.sql
+++ docs/sql/blogs.sql
@@ -1,17 +1,19 @@
+-- `go-admin`.blogs definition
+
 CREATE TABLE `blogs` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键编码',
   `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '名称',
-  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `author` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    `status` varchar(5) DEFAULT NULL,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题',
+  `author` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '作者',
+  `status` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '状态',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '内容',
   `created_at` datetime(3) DEFAULT NULL COMMENT '创建时间',
   `updated_at` datetime(3) DEFAULT NULL COMMENT '最后更新时间',
   `deleted_at` datetime(3) DEFAULT NULL COMMENT '删除时间',
   `create_by` bigint(20) DEFAULT NULL COMMENT '创建者',
   `update_by` bigint(20) DEFAULT NULL COMMENT '更新者',
-
   PRIMARY KEY (`id`),
   KEY `idx_tb_demo_create_by` (`create_by`) USING BTREE,
   KEY `idx_tb_demo_deleted_at` (`deleted_at`) USING BTREE,
   KEY `idx_tb_demo_update_by` (`update_by`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
